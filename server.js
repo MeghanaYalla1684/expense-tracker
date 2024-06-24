2d87f72135a01354a2f4ae62fd0e5b7cee9db6c7
Yalla Meghana <meghanayalla1@gmail.com>
2024-06-24 10:58:46 +0530

    New Changes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const connectDb = require("./config/connectDb");
+const path = require('path');
 
-// pzus8f4pLLHaahtA
 //config dotenc file
 dotenv.config();
 
@@ -27,6 +27,13 @@ app.use('/api/v1/transactions', require('./routes/transactionRoute'));
 
 //user routes
 app.use('/api/v1/users', require('./routes/userRoute'));
+
+//static files
+app.use(express.static(path.join(__dirname,'./client/build')));
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+});
 
 //port
 const PORT = 8080 || process.env.PORT;
